98d0d2469e5ccfa125b1fcbfb4619ce2018ed4c0
Andrew Saul <andres18@nssrstudio07.csa.scot.nhs.uk>
2022-03-03 09:14:43 +0000

    changes before modifying years in pops and myfunc as integers, for filtering purposes

--- scripts/my_funcs.R
+++ scripts/my_funcs.R
@@ -5,17 +5,18 @@ if(!require(librarian)){
 
 librarian::shelf(tidyverse, here, fs, readxl, janitor, glue, rlang)
 ###############################################################################
+## Scripts to acquire uptodate date from NRS #################################
+##############################################################################
 # fs::file_create(here::here("data"), "temp_jan_2021.csv")
 # capture_basename(here("data", "temp_jan_2021.csv"))
 # capture_recent_month_year(here("data", "temp_jan_2021.csv"))
 # 
 
-
 ## Function to capture url of data to be downloaded
+# url we are looking for contains string "beddays"
 scraplinks <- function(text_string = "beddays"){
   NRS_url  <- "https://publichealthscotland.scot/publications/delayed-discharges-in-nhsscotland-monthly/delayed-discharges-in-nhsscotland-monthly-figures-for-december-2021/#section-3"
-  text_string <- str_replace(text_string, pattern=" ", replacement="") %>% 
-    str_to_lower()
+  #text_string <- "beddays"
   # Create an html document from the url
   webpage <- xml2::read_html(NRS_url)
   # Extract the URLs
@@ -26,14 +27,16 @@ scraplinks <- function(text_string = "beddays"){
   #create df to store urls
   df <- data.frame(url = glue::glue("https://publichealthscotland.scot{url_}"))
   #extract the urls containing the specific text (text_string) and store in a vector
+  # url field is factor. need to convert to char field type
   hrefvec <- df %>% 
-    filter(str_detect(url, text_string)) %>% pull()
+    mutate(across(where(is.factor), as.character)) %>% 
+    filter(str_detect(url, text_string)) %>% unlist()
   #check that the vector contains a unique entry
   if (length(hrefvec) == 1){
     hrefvec
   } else {
     message("There is not a unique file containing the inputed text. Try adding 
-            more characters to the <text_string> object") 
+            more characters to the <text_string> object located within ") 
   }
 }
 
@@ -41,45 +44,173 @@ scraplinks <- function(text_string = "beddays"){
 download_data <- function(filepath_url, filename){
   
   if(missing(filepath_url)){
-    filepath_url <- scraplinks()
+    filepath_url <- scraplinks(filename)
   } # if statement
   
-  temp <- here::here("data", filename)
+  temp <- here::here("data", glue("{filename}.xlsx"))
   download.file(filepath_url, temp, mode="wb") #mode="wb" for binary excel files
 }
 
+##########################################################################
+###########functions for use in map_dfr for wide_data acquisition #####
 ###########################################################################
+
 # creates vector of sheet names dependent on pattern ie LAData or HBData
-get_sheetnames <- function(xl_file, pattern_string) {
-  sheet_names <- openxlsx::getSheetNames(file = xl_file) 
-    sheet_names[str_detect(sheet_names, pattern = pattern_string)]  
+get_sheetnames <- function(xl_file, sheet_names, pattern_string) {
+  sheet_names[str_detect(sheet_names, pattern = pattern_string)]  
 }
 
 # create vector of sheet names from which data is required 
-create_sheetname_vector <- 
-  function(xl_file = xl_file) {
-    partial_sheetnames <- c("LAData", "HBData")
-    map(partial_sheetnames, ~get_sheetnames(xl_file, .x)) %>% 
+# Only required if investigating "HBData" sheets as well
+
+# requires "get_sheetnames" function
+create_sheetname_vector <-   function(xl_file = xl_file) {
+    sheet_names <- openxlsx::getSheetNames(file = xl_file)
+    pattern_match <- c("LAData", "HBData")
+    map(pattern_match, ~get_sheetnames(xl_file, sheet_names, .x)) %>%
       unlist()
-  }
+}
+
+                ##########################################
+
+#takes first two digits of FY and converts it into calender year 20..
+convert_FY_to_calender <- function(tib){
+  tib %>% 
+    mutate (FY_begin = str_sub(FY, 1, 2)) %>%   # extracts last two digits from FY into <suffix year>
+    mutate(FY_begin = as.numeric(glue("20{FY_begin}")) ) # adds "20" in front of <suffix year>. Converts to numeric
+}
+
+# convert_calender_to_FY <- function(calyear){
+#   first_year <- str_sub(calyear, -2, -1) %>%  as.numeric() # extract last 2 digits from cal year into <first year>
+#   glue("{first_year}{first_year+1}") 
+# }
+
 # function that creates tibble for each sheet name, removes Qtr fields ,
 # adds Finanical Year field, creates type of authority field (LA or HB)
-# and renames LA/health board fields
+# renames LA/health board fields and filters "All" for AgeGroup
+
+#requires "convert_FY_to_calender" function
 create_FY_authority <- function(xl_file, sheet_name) {
   df<- read_excel(xl_file, sheet = sheet_name, range = "B1:U497")
   FY_var <- str_extract(sheet_name, pattern = "\\d+")
-  df %>% 
+  tempdf <- df %>% 
     select(-starts_with("Q")) %>% 
     rename(FYToDate = starts_with("FY_")) %>% 
+  #  mutate(Authority = if_else(str_detect(sheet_name, "HBData"), "HB", "LA")) %>% 
     drop_na(AgeGroup) %>% #discard these empty rows
-    mutate(FY = as.integer(FY_var)) %>% 
-    mutate(Authority = if_else(names(df)[1]=="LA", "LA", "HB")) %>% 
-    rename(Region = 1) 
+    mutate(FY = as.numeric(FY_var)) %>% 
+    rename(CouncilArea = 1) %>% 
+    convert_FY_to_calender() %>% 
+    filter(AgeGroup == "All") 
+ # if Health Board, only keeps Scotland element 
+  if(str_detect(sheet_name, "HBD")){
+   tempdf <-  tempdf %>% 
+      filter(CouncilArea == "Scotland")
+  } # if
+tempdf
+}
+
+
+#######################################################################################
+
+get_FY_CA_data <- function(tib) {
+  tib %>%
+    select(-c("FYToDate")) %>% 
+    pivot_longer(cols = all_of(month_order), names_to = "Months", values_to = "Counts") %>% 
+    mutate(Months = factor(Months, levels = c(month_order, ordered = TRUE))) %>% 
+    group_by(CouncilArea, ComplexNeedsFlag, FY, FY_begin, Months) %>% 
+    summarise(Counts = sum(Counts), .groups = "drop") 
 }
 
 
 
 
+#get bed rates (per 100,000) for each region, year or Month/year.
+# Rates dependent on pop of that specific year!
+# enter tibble, and element of ComplexNeedsFlag
+get_CA_bed_rates <- function(tib1, CNeeds_string, mon=NULL, fydate) {
+  tib1 <- tib1 %>% 
+    filter(ComplexNeedsFlag == CNeeds_string) %>% 
+    filter(FY %in% fydate)# %>% 
+    #filter(CouncilArea != "Other") 
+    
+    if (!is.null(mon)){
+      tib1 %>% filter(Months %in% mon) %>% 
+        # mutate(Months = factor(Months, levels = c(month_order, ordered = TRUE))) %>% 
+        left_join(LA_pop_18plus, by = c("CouncilArea", "FY_begin" = "year")) %>%
+        group_by(CouncilArea, FY, Months) %>% 
+        drop_na() %>% 
+        summarise(Counts = sum(Counts), Pop = mean(Pop), .groups = "drop") %>% 
+        mutate(Rate = Counts/Pop *100000)
+    } 
+  else {
+    left_join(tib1, LA_pop_18plus, by = c("CouncilArea", "FY_begin" = "year")) %>% 
+        group_by(CouncilArea, FY) %>% 
+        drop_na() %>% 
+        summarise(Counts = sum(Counts), Pop = mean(Pop), .groups = "drop") %>% 
+        
+        mutate(Rate = Counts/Pop *100000)
+  }
+}
+
+# gets bedrates for CCG Area or  Scotland
+
+#requires "get_CA_bed_rates" function
+get_group_bed_rates <- function(..., ggc_flag = FALSE, scot_flag = FALSE){
+
+  ggc_hcsps <- c("East Dunbartonshire", "East Renfrewshire", "Glasgow City", "Inverclyde",
+                 "Renfrewshire", "West Dunbartonshire")
+
+  if (scot_flag == TRUE) {
+    get_CA_bed_rates(...) %>%
+      filter(CouncilArea == "Scotland")
+  } else if (ggc_flag == TRUE ){
+    get_CA_bed_rates(...) %>%
+      filter(CouncilArea %in% ggc_hcsps) %>%
+      select(-c(FY)) %>%
+      summarise(Counts = sum(Counts), Pop = sum(Pop)) %>%
+      mutate(Rate = Counts/Pop *100000)
+  } else get_CA_bed_rates(...)
+
+}
+
+#################################################################################
+
+get_council_area_names <- function(tib){
+  tib %>% 
+    select(CouncilArea) %>% 
+    unique() %>% 
+    pull()
+}
+
+#get_prev5 selects last 6 years but excludes year 2021 . assumes earliest current year is 21
+get_prev5 <- function(tib, CA, CNeeds_string, fydate){
+  tib %>% 
+    mutate(prev5_flag = ifelse(fydate - FY <607 & fydate - FY > 0 & fydate!= 2021, TRUE, FALSE)) %>% 
+    filter(prev5_flag==TRUE) %>% 
+    select(!prev5_flag) %>% 
+    filter(ComplexNeedsFlag == CNeeds_string) %>% 
+    filter(CouncilArea == CA) %>% 
+    group_by(Months) %>% 
+    summarise(Min_value = min(Counts), Max_value = max(Counts), Avg = mean(Counts), .groups = "drop")
+}
+
+
+get_current_year_tib <- function(tib, CA, CNeeds_string, fydate){
+  tib %>% 
+    filter(ComplexNeedsFlag == CNeeds_string) %>% 
+    filter(FY %in% fydate) %>% 
+    filter(CouncilArea == CA) %>% 
+    drop_na(Counts)
+}
+
+
+create_5yr_plot <- function(dat1, dat2) {
+  ggplot(data=dat1, aes(x=Months,group=1))+
+    geom_ribbon(aes(ymin = Min_value, ymax=Max_value), fill = "grey70")+
+    geom_line(aes(y=Avg), linetype = "dashed")+
+    geom_line(data = dat2, aes(y=Counts), group = 1)
+} 
 #############################################################################
 ########### Population data###################################################
 # #########################################################################
--- scripts/populations.R
+++ scripts/populations.R
@@ -1,50 +1,86 @@
-source(here::here("scripts", "my_funcs.R"))
+#the pops .rds file does not contain Scotland pop data
 
-# cleans excel file into long table
-clean_xlfile <- function(xlpathname, sex) {
-  read_excel(xlpathname, range = "A3:CQ6981") %>% 
-    clean_names(case="big_camel") %>% 
-    mutate(sex = sex) %>% 
-    slice(c(1,3:6981)) %>% 
-    mutate(CouncilArea = if_else(AreaName == "SCOTLAND", "SCOTLAND", CouncilArea)) %>% 
-    select(-c(4:22)) %>% 
-    pivot_longer(cols = c(4:76), names_to = "Age", values_to = "Pop")
+
+  
+###########################################################################################
+#create pop table for Council Areas
+getLAPops <- function(age_lowest = 0){
+  pops <- readRDS(paste0("/conf/linkage/output/lookups/Unicode/Populations/",
+                         "Estimates/CA2019_pop_est_1981_2020.rds")) 
+  
+  lkp <- read.csv(glue("{here::here()}/../Ad Hoc/DD_BedDays_Lockdown/DD_Bed Days - R Project/scripts/helper/la_lookup.csv")) %>%
+    rename(ca2019name = NRS, CouncilArea = PHS)
+ 
+  suppressWarnings(
+    pops <- pops %>% filter(age >= age_lowest) %>%
+      group_by(ca2019name, year) %>%
+      summarise(Pop = sum(pop), .groups = "drop") %>%
+      left_join(lkp, "ca2019name") %>%
+      select(CouncilArea, year, Pop) %>%
+      mutate(CouncilArea = as.character(CouncilArea))
+    )
+  
+  pops <- pops %>% 
+    group_by(year) %>% 
+    summarise(Pop = sum(Pop)) %>% 
+    add_column(CouncilArea = "Scotland") %>% 
+    bind_rows(pops) 
+  
+ # as mid-2021 doesn't yet exist, copy mid 2020 pop and rename it as year 2021
+   pops %>% 
+     filter(year == 2020) %>% #isolate year 2020
+      mutate(year = 2021) %>% # rename values 2021
+      bind_rows(pops)
+   
+   
 }
-##############NRS urls ##########################################
-urlmales2020 <- "https://www.nrscotland.gov.uk/files//statistics/population-estimates/sape-time-series/males/sape-2020-males.xlsx"
-urlfemales2020 <- "https://www.nrscotland.gov.uk/files//statistics/population-estimates/sape-time-series/females/sape-2020-females.xlsx"
-
-####################download data from urls for males and females################################
-
-
-download_data(urlmales2020,"males_pop.xlsx")
-download_data(urlfemales2020,"females_pop.xlsx")
-
-male_pop_path <- here::here("data","males_pop.xlsx")
-female_pop_path <- here::here("data","females_pop.xlsx")
-
-
-
-male_pop_2020 <- clean_xlfile(male_pop_path, "Male")
-female_pop_2020 <- clean_xlfile(female_pop_path, "Female")
-
-
-## raw_pop_2020 combines male and female age group pops 
-raw_pop_2020 <- bind_rows(male_pop_2020, female_pop_2020) %>% 
-  drop_na(AreaName) #No rows dropped!
-
-#delete unwanted tibbles
-rm(male_pop_2020, female_pop_2020)
-
-# vector "Council_areas" containing LA areas
-Council_areas <- raw_pop_2020 %>% 
-  select(CouncilArea) %>% 
-  unique() %>% 
-  pull()
-
-# Population 18+ in "Council_areas"
-LA_pop_18plus <- raw_pop_2020 %>% 
-  group_by(CouncilArea) %>% 
-  summarise(Pop = sum(Pop))
-
-
+# 
+# ##############NRS urls ##########################################
+# urlmales2020 <- "https://www.nrscotland.gov.uk/files//statistics/population-estimates/sape-time-series/males/sape-2020-males.xlsx"
+# urlfemales2020 <- "https://www.nrscotland.gov.uk/files//statistics/population-estimates/sape-time-series/females/sape-2020-females.xlsx"
+# 
+# ####################download data from urls for males and females################################
+# download_pop_data <- function() {
+#   download_data(urlmales2020,"males_pop.xlsx")
+#   download_data(urlfemales2020,"females_pop.xlsx")
+# }
+# 
+# # cleans excel file into long table
+# clean_xlfile <- function(xlpathname, sex) {
+#   read_excel(xlpathname, range = "A3:CQ6981") %>% 
+#     clean_names(case="big_camel") %>% 
+#     mutate(sex = sex) %>% 
+#     slice(c(1,3:6981)) %>% 
+#     mutate(CouncilArea = if_else(AreaName == "SCOTLAND", "Scotland", CouncilArea)) %>% 
+#     select(-c(4:22)) %>% 
+#     pivot_longer(cols = c(4:76), names_to = "Age", values_to = "Pop")
+# }
+# 
+# change_CA_name <- function(tib, orig_name, dest_name) {
+#   tib %>% 
+#     mutate(CouncilArea = if_else(CouncilArea == orig_name, dest_name, CouncilArea))
+# }
+# 
+# 
+# create_population_tibble <- function(...) {
+# 
+#   male_pop_path <- here::here("data","males_pop.xlsx")
+#   female_pop_path <- here::here("data","females_pop.xlsx")
+#   
+#   male_pop_2020 <- clean_xlfile(male_pop_path, "Male")
+#   female_pop_2020 <- clean_xlfile(female_pop_path, "Female")
+#   
+#   
+#   ## raw_pop_2020 combines male and female age group pops 
+#   bind_rows(male_pop_2020, female_pop_2020) %>% 
+#     drop_na(AreaName) #No rows dropped!
+# }
+# 
+# # get Population 18+ in "Council_areas"
+# get_CA_pop <- function(dat){
+#   dat %>% 
+#     group_by(CouncilArea) %>% 
+#     summarise(Pop = sum(Pop))
+# }
+# 
+# 
